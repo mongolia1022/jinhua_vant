2b6a2cc49e2d41170b573a5cfa85e1824c657c16
BOZKOGO <944099408@qq.com>
2019-12-13 09:59:07 +0800

    1.把活动产品详细页倒数放到了普通产品详细页里面            2.我的订单tab修改

--- jinhua_vant/pages/pro_details/pro_details.js
+++ jinhua_vant/pages/pro_details/pro_details.js
@@ -27,6 +27,10 @@ Page({
     //弹出层
     show: false,
 
+    //倒计时
+    listData: [], 
+
+
     //选择规格
     columns: ['500g*15包/箱', '600g*15包/箱', '700g*15包/箱', '800g*15包/箱', '900g*15包/箱'],
       productData:{}
@@ -86,6 +90,61 @@ Page({
    */
   onLoad: function (options) {
       this.goods_info(options.id);
+      //倒计时
+      let list = [
+        {
+          id: 1,
+          memberNickname: '列表1',
+          remainTime: 86400000
+        }
+      ];
+    this.setData({
+      listData: list
+    });
+    this.setCountDown();
+  },
+
+  /*倒计时*/
+  setCountDown: function () {
+    let time = 100;
+    let { listData } = this.data;
+    let list = listData.map((v, i) => {
+      if (v.remainTime <= 0) {
+        v.remainTime = 0;
+      }
+      let formatTime = this.getFormat(v.remainTime);
+      v.remainTime -= time;
+      v.countDown = `${formatTime.hh}:${formatTime.mm}:${formatTime.ss}.${parseInt(formatTime.ms / time)}`;
+      v.countDownhh = `${formatTime.hh}`;
+      v.countDownmm = `${formatTime.mm}`;
+      v.countDownss = `${formatTime.ss}`;
+      return v;
+    })
+    this.setData({
+      listData: list
+    });
+    setTimeout(this.setCountDown, time);
+  },
+  /**
+     * 格式化时间
+     */
+  getFormat: function (msec) {
+    let ss = parseInt(msec / 1000);
+    let ms = parseInt(msec % 1000);
+    let mm = 0;
+    let hh = 0;
+    if (ss > 60) {
+      mm = parseInt(ss / 60);
+      ss = parseInt(ss % 60);
+      if (mm > 60) {
+        hh = parseInt(mm / 60);
+        mm = parseInt(mm % 60);
+      }
+    }
+    ss = ss > 9 ? ss : `0${ss}`;
+    mm = mm > 9 ? mm : `0${mm}`;
+    hh = hh > 9 ? hh : `0${hh}`;
+    return { ms, ss, mm, hh };
   },
 
   /**
